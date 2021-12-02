54611eed65d43d2d2a9d98a97af142451ce53a1d
industriallife <industriallife@live.ru>
2021-12-03 00:05:49 +0500

    1st update

--- main.py
+++ main.py
@@ -14,17 +14,14 @@ def start_message(message):
 def send_text(message):
     if message.text.lower() == 'прости':
         url = 'https://api.scryfall.com/cards/e9d5aee0-5963-41db-a22b-cfea40a967a3?format=image'
-        #img = Image.open(urlopen(url))
-
-
         bot.send_photo(message.chat.id,url)
         #bot.send_document(message.chat.id, img)
-   # elif message.text.lower() == 'наш второй смайлик':
-    #    img = open('Смайлики и люди 2.png', 'rb')
-     #   bot.send_document(message.chat.id, img)
+    elif message.text.lower() == 'наш второй смайлик':
+        url = 'https://api.scryfall.com/cards/00006596-1166-4a79-8443-ca9f82e6db4e?format=image'
+        bot.send_document(message.chat.id, url)
 
     else:
-        bot.send_message(message.chat.id, 'Прости, но пока у меня нет этих Emoji')
+        bot.send_message(message.chat.id, 'Прости, но пока у меня нет этих карт')
 
 
 bot.polling()
