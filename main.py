b76329ddb26db07c26aa73411d3802a50363d0bc
Alexander Lovchev <alexander.lovchev@sm-analytics.com>
2022-07-21 11:15:37 +0500

    update text

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ def start_message(message):
 
 
 def send_text(message):
-    if message.text.lower() == 'прости':
+    if message.text.lower() == 'test1':
         url = 'https://api.scryfall.com/cards/e9d5aee0-5963-41db-a22b-cfea40a967a3?format=image'
         bot.send_photo(message.chat.id,url)
         #bot.send_document(message.chat.id, img)
-    elif message.text.lower() == 'наш второй смайлик':
+    elif message.text.lower() == 'test2':
         url = 'https://api.scryfall.com/cards/00006596-1166-4a79-8443-ca9f82e6db4e?format=image'
-        bot.send_document(message.chat.id, url)
+        bot.send_photo(message.chat.id, url)
 
     else:
         bot.send_message(message.chat.id, 'Прости, но пока у меня нет этих карт')
